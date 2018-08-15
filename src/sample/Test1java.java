682971a6a4f86e0d6c332e602f889d2604461e46
SANDEEP <togiti.sandeep1@gmail.com>
2018-08-14 20:55:44 -0500

    Created new QA Branch

---
+++ src/sample/Test1java.java
@@ -0,0 +1,16 @@
+package sample;
+
+public class Test1java {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a = 10;
+		int b = 20;
+		int c = a+b;
+		System.out.println(c);
+	}
+	
+	
+
+}
